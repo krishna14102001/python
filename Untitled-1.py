cbca8a2bc07d9e1895b4ccfac77a617bff861a98
krishna14102001 <88551584+krishna14102001@users.noreply.github.com>
2021-09-04 18:04:22 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,6 @@
+print("multiple of current num and previous num")
+previousnumb=0
+for i in range(5):
+    ans=previousnumb*i
+    print("current number",i,"previous number",previousnumb,"multi",ans)
+    previousnumb=i
